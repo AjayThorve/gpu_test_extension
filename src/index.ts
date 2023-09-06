90b8eea78a94ce03a35dc8e0b078b418c299326e
Ajay Thorve <ajaythorve1994@gmail.com>
2023-09-05 20:45:49 -0700

    add widget tracker to track widget state

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import {
   ILabShell,
+  ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import { ControlWidget } from './launchWidget';
+import { MainAreaWidget, WidgetTracker } from '@jupyterlab/apputils';
 
 /**
  * Initialization data for the react-widget extension.
@@ -12,10 +14,27 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'react-widget',
   description: 'A minimal JupyterLab extension using a React Widget.',
   autoStart: true,
-  optional: [ILabShell],
-  activate: (app: JupyterFrontEnd, labShell: ILabShell) => {
-    const controlWidget = new ControlWidget(app, labShell);
+  requires: [ILabShell],
+  optional: [ILayoutRestorer],
+  activate: (
+    app: JupyterFrontEnd,
+    labShell: ILabShell,
+    restorer: ILayoutRestorer | null
+  ) => {
+    const tracker = new WidgetTracker<MainAreaWidget>({
+      namespace: 'gpu-dashboard-widgets'
+    });
+    const controlWidget = new ControlWidget(app, labShell, tracker);
     controlWidget.id = 'gpu-dashboard';
+    if (restorer) {
+      restorer.add(controlWidget, 'gpu-dashboard');
+      restorer.restore(tracker, {
+        command: 'gpu-dashboard-widget:open',
+        args: widget => ({ label: widget.title.label }),
+        name: widget => widget.title.label
+      });
+    }
+
     labShell.add(controlWidget, 'left', { rank: 200 });
   }
 };
--- src/launchWidget.tsx
+++ src/launchWidget.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { ILabShell, JupyterFrontEnd } from '@jupyterlab/application';
 import { ReactWidget, Button } from '@jupyterlab/ui-components';
 import { GpuUsageChartWidget, GpuUtilizationChartWidget } from './charts';
-import { MainAreaWidget } from '@jupyterlab/apputils';
+import { MainAreaWidget, WidgetTracker } from '@jupyterlab/apputils';
 
 interface IControlProps {
   app: JupyterFrontEnd;
   labShell: ILabShell;
+  tracker: WidgetTracker;
 }
 
 interface IWidgetInfo {
@@ -14,22 +15,40 @@ interface IWidgetInfo {
   instance: MainAreaWidget;
 }
 
-const Control: React.FC<IControlProps> = ({ app, labShell }) => {
+const Control: React.FC<IControlProps> = ({ app, labShell, tracker }) => {
   const openWidgets: IWidgetInfo[] = [];
+
+  app.commands.addCommand('gpu-dashboard-widget:open', {
+    label: 'Open GPU Dashboard Widget',
+    execute: args => {
+      const { label } = args as { label: string };
+      const w = tracker.find(widget => widget.title.label === label);
+      if (w) {
+        if (!w.isAttached) {
+          labShell.add(w, 'main');
+        }
+        labShell.activateById(w.id);
+        return;
+      }
+      openWidgetByLabel(label);
+    }
+  });
 
   const openWidget = (widgetCreator: () => ReactWidget, label: string) => {
     // Check if a widget with the same label is already open
     const existingWidget = openWidgets.find(widget => widget.label === label);
-
     if (existingWidget) {
       // If widget is already open, bring it to the front
       labShell.activateById(existingWidget.instance.id);
+      tracker.add(existingWidget.instance);
     } else {
       // If widget is not open, create and add it
       const content = widgetCreator();
       const widgetInstance = new MainAreaWidget({ content });
       widgetInstance.title.label = label;
       app.shell.add(widgetInstance, 'main');
+      tracker.add(widgetInstance);
+      console.log(tracker);
       openWidgets.push({ label, instance: widgetInstance });
 
       // Remove the widget from openWidgets when it is closed
@@ -42,24 +61,28 @@ const Control: React.FC<IControlProps> = ({ app, labShell }) => {
     }
   };
 
-  const openGPUUsageWidget = () => {
-    openWidget(() => new GpuUsageChartWidget(), 'GPU Usage Widget');
-  };
-
-  const openGPUUtilizationWidget = () => {
-    openWidget(() => new GpuUtilizationChartWidget(), 'GPU Utilization Widget');
+  const openWidgetByLabel = (label: string) => {
+    openWidget(
+      label === 'GPU Usage Widget'
+        ? () => new GpuUsageChartWidget()
+        : () => new GpuUtilizationChartWidget(),
+      label
+    );
   };
 
   return (
     <div className="gpu-dashboard-container">
       <div className="gpu-dashboard-header">GPU Dashboards</div>
       <hr className="gpu-dashboard-divider" />
-      <Button className="gpu-dashboard-button" onClick={openGPUUsageWidget}>
+      <Button
+        className="gpu-dashboard-button"
+        onClick={() => openWidgetByLabel('GPU Usage Widget')}
+      >
         Open GPU Usage Widget
       </Button>
       <Button
         className="gpu-dashboard-button"
-        onClick={openGPUUtilizationWidget}
+        onClick={() => openWidgetByLabel('GPU Utilization Widget')}
       >
         Open GPU Utilization Widget
       </Button>
@@ -70,13 +93,17 @@ const Control: React.FC<IControlProps> = ({ app, labShell }) => {
 export class ControlWidget extends ReactWidget {
   constructor(
     private app: JupyterFrontEnd,
-    private labShell: ILabShell
+    private labShell: ILabShell,
+    private tracker: WidgetTracker
   ) {
     super();
     this.addClass('jp-ControlWidget');
+    this.tracker = tracker;
   }
 
   render(): JSX.Element {
-    return <Control app={this.app} labShell={this.labShell} />;
+    return (
+      <Control app={this.app} labShell={this.labShell} tracker={this.tracker} />
+    );
   }
 }
